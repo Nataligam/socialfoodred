ea12973ca306e09583c58f9eb7f7ed50c82dd8c1
gabriel contreras <gabrielcontreras1426@gmail.com>
2018-06-20 20:36:11 -0500

    Lista la plantilla de receta

--- src/views/Receta.vue
+++ src/views/Receta.vue
@@ -51,8 +51,10 @@
                              <div class="mt-3">
                                  <ul class="list-group">
                                   <label>Ingredientes agregados:</label>                                  
-                                  <li class="list-group-item">Ingrediente 1<button class="btn btn-outline-danger btnIzq"><span class="icon-close"></span></button></li>
-                                  <li class="list-group-item">Ingrediente 2<button class="btn btn-outline-danger btnIzq"><span class="icon-close"></span></button></li>                     
+                                  <li v-for="(ingrediente, index) in ingredientes" v-bind:key="ingrediente.nombre" class="list-group-item">
+                                      {{ingrediente.nombre }} {{ingrediente.cantidad }} {{ingrediente.unidad}}
+                                      <button @click="eliminarIng(index)" class="btn btn-outline-danger btnIzq"><span class="icon-close"></span></button>
+                                </li>
                               </ul>
                           </div>
                           <!--Lista de ingredientes-->                               
@@ -61,37 +63,88 @@
                   <div class="form-group">
                     <h5>Pasos:</h5>
                     <div class="borde p-3">
-                        <div class="row">
-                            <div class="col-5">
+                        <div class="row ">
+                            <div class="form-group col-12">
                                 <label >Nombre:</label>
                                 <input  v-model="nombrepaso" type="text" class="form-control" placeholder="Nombre del paso">
                             </div>
-                            <div class="col-5">
+                            <div class=" form-group col-12">
+                                <label >Ingredientes:</label>
+                                <select v-model="parametros" multiple class="form-control">
+                                <option v-for="ingrediente in ingredientes" v-bind:key="ingrediente.nombre" >{{ingrediente.nombre }} {{ingrediente.cantidad }} {{ingrediente.unidad}}</option>
+                                
+                                </select>
+                            </div>
+                            <div class="form-group col-10">
                                 <label >Descripción:</label>
-                                <textarea  v-model="descripcionpaso" class="form-control" rows="1"></textarea>
+                                <textarea  v-model="descripcionpaso" class="form-control" rows="2"></textarea>
                             </div>
                             <div class="col-2">
                                 <label>Añadir:</label>
-                             <button class="btn btn-outline-info2" type="submit"><span class="icon-plus"></span></button>
+                             <button class="btn btn-outline-info2" type="submit" v-on:click="agregarpasos"><span class="icon-plus"></span></button>
                          </div>                        
                      </div>
                      <!--Lista de pasos-->
                      <div class="mt-3">
                          <ul class="list-group">
                           <label>Pasos agregados:</label>                                  
-                          <li class="list-group-item">Paso1<button class="btn btn-outline-danger btnIzq"><span class="icon-close"></span></button></li>
-                          <li class="list-group-item">Paso2<button class="btn btn-outline-danger btnIzq"><span class="icon-close"></span></button></li>                     
+                           <li v-for="(paso, index) in pasos" v-bind:key="paso.nombre" class="list-group-item">
+                                      {{paso.nombre }} 
+                                <button @click="eliminarpaso(index)" class="btn btn-outline-danger btnIzq"><span class="icon-close"></span></button>
+                            </li>
                       </ul>
                   </div>
                   <!--Lista de pasos--> 
               </div>
           </div>
-          <button type="submit" class="btn btn-outline-info2 mb-5 btnIzq">VER RECETA</button>     
-      </form>
+          <!-- Button trigger modal -->
+<button type="button" class="btn btn-outline-info2 mb-5 btnIzq" data-toggle="modal" data-target="#exampleModal">
+  Vista previa
+</button>
+
+<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Mi receta</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+       
+           <div class="row">
+              
+            <p> <span class="recervada">Package</span> &nbsp;&nbsp;&nbsp;Receta<br><br>
+            <span class="recervada">public class </span> &nbsp;&nbsp; {{titulo}} {<br><br>
+              
+            <span v-for="ingrediente in ingredientes" v-bind:key="ingrediente.nombre">
+            <span class="recervada">private String</span>=&nbsp;"{{ingrediente.nombre}}  {{ingrediente.cantidad}}  {{ingrediente.unidad}}";<br></span><br>
+              
+            <span v-for="paso in pasos" v-bind:key="paso.nombre">
+            <span class="recervada">public void </span>&nbsp;{{paso.nombre}} (<span v-for="(parametro, index) in paso.parametro" v-bind:key="parametro">
+                <span v-if="Object.keys(parametros).length-1 > index" > String {{parametro}}, </span>
+                <span v-else > String {{parametro}} </span> </span>) { <br>
+              //  {{paso.descripcion}} <br>
+            }<br></span>
+            }
+             </p>
+  
+        </div>   
+                     
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-outline-info2 mb-5 btnIzq" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-outline-info2 mb-5 btnIzq">Publicar</button>
+      </div>
+    </div>
   </div>
 </div>
-<input v-model="message" placeholder="edit me">
-							<p>Message is: {{ message }}</p>
+</form>
+  </div>
+</div>
+                    
 							
 
             </div>                           
@@ -110,25 +163,46 @@ export default{
 	name:'Receta',
 	data(){
 		return {
-
             ingredientes:[],
-            titulo,
-            nombreing,
-            cantidading,
-            unidading,
-            nombrepaso,
-            descripcionpaso
-
+            pasos:[],
+            titulo:'',
+            selected:'',
+            nombreing:'',
+            cantidading:'',
+            unidading:'',
+            nombrepaso:'',
+            parametros:'',
+            descripcionpaso:''
 
 		}
     },
     methods:{
 		 agregarIng(){
-         this.ingredientes.push({nombre:this.nombreing, cantidad:this.cantidading,unidad:this.unidading});
+             
+         this.ingredientes.push({nombre:this.nombreing, cantidad:this.cantidading, unidad:this.unidading});
+
          this.nombreing='';
          this.cantidading='';
          this.unidad='';
-         }
+         },
+         agregarpasos(){
+            console.log(this.ingredientes)
+         this.pasos.push({nombre:this.nombrepaso, parametro:this.parametros, descripcion:this.descripcionpaso});
+        
+         this.nombrepaso='';
+         this.descripcionpaso='';
+         
+         },
+        eliminarIng(index){
+             
+         this.ingredientes.splice(index,1);
+        
+         },
+         eliminarpaso(index){
+             
+         this.pasos.splice(index,1);
+        
+         },
     },
 	components:{
 		DefaultLayout,
@@ -156,6 +230,10 @@ export default{
 
 .borde{
 	border: 2px #48dbfb solid;
+}
+
+.recervada{
+    color: blue;
 }
 
 .divpublicar{
