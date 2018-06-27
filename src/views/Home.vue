9e14af456e62af4aacf0074595ded229d05e5571
gabriel contreras <gabrielcontreras1426@gmail.com>
2018-06-26 19:09:01 -0500

    categorias y privacidad

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -65,7 +65,7 @@ export default{
 	methods:{
 		validar(){
 			console.log("memo");
-			axios.get('http://fd755025.ngrok.io/v1/usuario/1',{
+			axios.get('http://600a25ce.ngrok.io/v1/usuario/1',{
 
 			})
 			.then(response =>{
@@ -74,7 +74,7 @@ export default{
 		},
 		save(){
 			console.log("memosave");
-			axios.post('http://fd755025.ngrok.io/v1/usuario',{
+			axios.post('http://600a25ce.ngrok.io/v1/usuario',{
 
 				nickname:'pedro',
 				correo:'pedrolop90789.gmail.com',
@@ -88,7 +88,7 @@ export default{
 		actualizar(){
 			console.log("memoedit");
 			console.log(this.usuario)
-			axios.put('http://fd755025.ngrok.io/v1/usuario/6',{
+			axios.put('http://600a25ce.ngrok.io/v1/usuario/6',{
 
 				nickname:'benito camelas',
 				correo:'benito.gmail.com',
@@ -102,7 +102,7 @@ export default{
 		 // METODOS QUE SI SIRVEN PARA EL MODELO USUARIO
 		 registrarUsuario(){
 		 	console.log('registrar')
-		 	axios.post('http://58c09312.ngrok.io/v1/usuario',{
+		 	axios.post('http://600a25ce.ngrok.io/v1/usuario',{
 
 		 		nickname:this.usuario,
 		 		correo:this.correo,
@@ -115,7 +115,7 @@ export default{
 		 },
 		login(){
 		 console.log('login')
-			 axios.post('http://58c09312.ngrok.io/v1/usuario/login',{
+			 axios.post('http://600a25ce.ngrok.io/v1/usuario/login',{
 			 correo:this.username,
 			 password:this.password
 			})
@@ -141,7 +141,7 @@ export default{
 		  buscarAmigo(){
 		  console.log("buscaramigo");
 			 var buscar = 'pedro'
-			axios.get('http://baab64d5.ngrok.io/v1/usuarios/' + buscar,{
+			axios.get('http://600a25ce.ngrok.io/v1/usuarios/' + buscar,{
 			})
 			.then(response =>{
 			 console.log(response);
